7527817b8d59b08b523a260a15b5cf73afe90657
Craig R. Hughes <craig.git@rungie.com>
2009-03-16 13:10:43 -0700

    Update getforecast to calculate minimum and log stuff

--- getforecast.pl
+++ getforecast.pl
@@ -4,7 +4,7 @@ use WWW::Mechanize;
 
 my $mech = WWW::Mechanize->new;
 
-$mech->get('http://forecast.weather.gov/MapClick.php?w0=t&FcstType=digital&textField1=36.70526&textField2=-121.32623&menu=1');
+$mech->get('http://forecast.weather.gov/MapClick.php?w0=t&FcstType=digital&textField1=36.704788132826586&textField2=-121.32691383361816&menu=1');
 
 use HTML::TableExtract;
 
@@ -23,8 +23,23 @@ foreach my $ts ($te->tables) {
 	}
 }
 
+# Find lowest forecast temp from midnight through 8am
+my $min_temp = "00";
+foreach my $i (qw (01 02 03 04 05 06 07 08)) {
+	$min_temp = $i if $temp_hash{$i} <= $temp_hash{$min_temp};
+}
+
 use Data::Dumper;
 
 $Data::Dumper::Sortkeys = 1;
+$Data::Dumper::Indent = 0;
 
 print Dumper \%temp_hash;
+print "\nMin temp is $temp_hash{$min_temp} at $min_temp o'clock\n";
+
+open(LOG,">>forecast_data.log");
+
+use Date::Manip;
+
+print LOG ParseDate('now'),"\tForecast min is $temp_hash{$min_temp} at $min_temp o'clock\n";
+print LOG ParseDate('now'),"\t",Dumper(\%temp_hash),"\n";
