261f0335da0e7589bd9492b0ca8c98c406ba4506
Craig R. Hughes <craig.git@rungie.com>
2015-10-13 15:58:39 -0700

    Make a web page that renders stuff

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,41 @@
-var influx = require('influx')({host:'localhost',database:'weather'});
-var router = require('express').Router();
+var influx    = require('influx')({host:'weather-pi.rungie.com',database:'weather'});
+var router    = require('express').Router();
+var moment    = require('moment');
+var promisify = require('es6-promisify');
+var Lazy      = require('lazy.js');
 
-influx.getDatabaseNames(function(err,dbnames)
-{
-    if(err)
-    {
-        console.error(err);
-    }
-    else
-    {
-        dbnames.forEach(function(name)
-    {
-        console.log("Database:",name);
-    });
-    }
-});
+var influxQuery = promisify(influx.query.bind(influx));
 
 router.get('/', function(req, res, next)
 {
     res.render('index', { title: 'Express' });
+});
+
+router.get('/weather_data', function(req, res, next)
+{
+    var maxDate = moment('2015-04-01');
+    var minDate = moment(maxDate).subtract(30, 'days');
+
+    var query = 'select min(CHILL) as temp_low, percentile(CHILL,40) as temp_lowmid, median(CHILL) as temp_mid, percentile(CHILL,60) as temp_highmid, max(CHILL) as temp_high,'+
+                       'min(HUM)    as hum_low, percentile(HUM,40)    as hum_lowmid, median(HUM)   as hum_mid,  percentile(HUM,60)   as hum_highmid,  max(HUM)   as hum_high,'+
+                       'min(BARO)  as baro_low, percentile(BARO,40)  as baro_lowmid, median(BARO)  as baro_mid, percentile(BARO,60)  as baro_highmid, max(BARO)  as baro_high'+
+                    ' from station where time > \''+minDate.format('YYYY-MM-DD HH:mm:ss')+'\' and time < \''+maxDate.format('YYYY-MM-DD HH:mm:ss')+
+                    '\' group by time(1d) fill(none)';
+    console.log('Query:',query);
+    influxQuery(query)
+    .then(function(results)
+    {
+        res.status(200).json({
+            from: minDate._d,
+            to:   maxDate._d,
+            data: results[0].sort(function(a,b){ return a.time.localeCompare(b.time); })
+        });
+    })
+    .then(undefined, function(err)
+    {
+        console.error('Error:',err.stack);
+        res.status(500).json({error: err.stack});
+    });
 });
 
 module.exports = router;
--- src/web.js
+++ src/web.js
@@ -4,7 +4,6 @@ var https         = require('https');
 var express       = require('express');
 var path          = require('path');
 var favicon       = require('serve-favicon');
-var logger        = require('morgan');
 var cookieParser  = require('cookie-parser');
 var bodyParser    = require('body-parser');
 var compression   = require('compression');
